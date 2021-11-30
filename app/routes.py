89d4f1a5ef4ba07aa813a1d35118e7c5e4103cb9
Norman Staub <normanjstaub@gmail.com>
2021-11-30 11:39:48 -0500

    stuff done

---
+++ app/routes.py
@@ -0,0 +1,25 @@
+from datetime import datetime
+from flask import render_template, flash, redirect, url_for, request
+from flask_login import login_user, logout_user, current_user, login_required
+from werkzeug.urls import url_parse
+from app import app, db
+from app.forms import --
+from app.models import --
+
+@app.before_request
+def before_request():
+    if current_user.is_authenticated:
+        current_user.last_seen = datetime.utcnow()
+        db.session.commit()
+
+@app.route('/')
+@app.route('/home')
+def home():
+    return(render_template('home.html', user=user))
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+    form = LoginForm()
+    if form.validate_on_submit
