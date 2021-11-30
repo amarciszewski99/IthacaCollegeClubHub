d6a7e266768ac72f0aa99af17b17b1cb79bdfe26
Adam Marciszewski <amarciszewski@ithaca.edu>
2021-11-30 11:53:52 -0500

    adam do thing

--- app/__init__.py
+++ app/__init__.py
@@ -7,9 +7,13 @@ from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_bootstrap import Bootstrap
 from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 app.config.from_object(Config)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
